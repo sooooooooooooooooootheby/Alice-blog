0f3aa4d344b18132248108f1a97389e3ca68427a
Ririsu-Kokoromu <3383281160@qq.com>
2023-12-29 14:27:27 +0800

    修改了所学的样式

--- themes/MyTheme/source/js/title.js
+++ themes/MyTheme/source/js/title.js
@@ -1,15 +1,27 @@
 var originTitle = document.title;
 var titleTime;
-document.addEventListener('visibilitychange', function () {
+document.addEventListener("visibilitychange", function () {
     if (document.hidden) {
-        document.title = '页面丢失 x_x  |' + originTitle;
+        document.title = "页面丢失 x_x  |" + originTitle;
         if (titleTime != null) {
             clearTimeout(titleTime);
         }
     } else {
-        document.title = '～(∠・ω< )⌒ 骗你的～  |' + originTitle;
+        document.title = "～(∠・ω< )⌒ 骗你的～  |" + originTitle;
         titleTime = setTimeout(function () {
             document.title = originTitle;
         }, 2000);
     }
 });
+let msg = "%c 💚 Our site is very new. Let us know if you find any issues 💚";
+let styles = [
+    "font-size: 12px",
+    "color: #fffce1",
+    "font-family: monospace",
+    "background: #0e100f",
+    "display: inline-block",
+    "padding: 1rem 3rem",
+    "border: 1px solid #fffce1",
+    "border-radius: 4px;",
+].join(";");
+console.log(msg, styles);
