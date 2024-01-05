58ddafff33a31203f9de4d711ad5820d02d216e8
Ririsu-Kokoromu <3383281160@qq.com>
2024-01-05 23:27:13 +0800

    修复中键到顶部导航栏不显示问题

--- themes/MyTheme/source/js/header.js
+++ themes/MyTheme/source/js/header.js
@@ -6,3 +6,15 @@ window.onmousewheel=document.onmousewheel=(e)=>{
         header.style.opacity = 1;
     } 
 }
+
+window.onscroll = function() {
+    // 获取滚动条的垂直偏移量
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    // 判断滚动条是否在顶部
+    if (scrollTop === 0) {
+        header.style.opacity = 1;
+    } else {
+        header.style.opacity = 0;
+    }
+};
