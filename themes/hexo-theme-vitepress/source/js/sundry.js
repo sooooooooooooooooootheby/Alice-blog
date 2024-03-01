1dc975641e3269e80926b4b1c4b077bf5ee8e4fe
Riririsu-Kokoromu <therese.sjdyka@gmail.com>
2024-03-01 11:09:05 +0800

    添加 访客数据

---
+++ themes/hexo-theme-vitepress/source/js/sundry.js
@@ -0,0 +1,35 @@
+var deviceOS = '';
+var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+if (/android/i.test(userAgent)) {
+    deviceOS = "Android";
+} else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    deviceOS = "iOS";
+} else if (/Win64|Win32|Win16/.test(userAgent)) {
+    deviceOS = "Windows";
+} else if (/Macintosh/.test(userAgent)) {
+    deviceOS = "MacOS";
+} else if (/Linux/.test(userAgent)) {
+    deviceOS = "Linux";
+}
+
+var deviceBrowser = '';
+var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+if (/OPR/i.test(userAgent)) {
+    deviceBrowser = "Opera";
+} else if (/Chrome/i.test(userAgent)) {
+    deviceBrowser = "Chrome";
+} else if (/Firefox/i.test(userAgent)) {
+    deviceBrowser = "Firefox";
+} else if (/Safari/i.test(userAgent)) {
+    deviceBrowser = "Safari";
+} else if (/MSIE/i.test(userAgent) || !!document.documentMode == true) {
+    deviceBrowser = "IE";
+} else {
+    deviceBrowser = "Unknown";
+}
+
+let welcome = deviceOS + '的' + deviceBrowser
+let device = document.getElementById('device');
+device.innerHTML = welcome
